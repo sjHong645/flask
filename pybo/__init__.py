e31621d61c59c9620d643a05ab05593836c61663
sponbob-pat <ixdos1111@gmail.com>
2024-05-13 17:40:27 +0900

    chapter2-4 1. flask ORM 라이브러리 사용하기

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,26 +1,22 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-# 기존 pybo 함수 코드 
-""" app = Flask(__name__)
+import config
 
-@app.route('/')
-def hello_pybo() : 
-    return 'Hello, Pybo!' """
-
-# 애플리케이션 팩토리 적용
-""" def create_app() : 
-    app = Flask(__name__)
-
-    @app.route('/')
-    def hello_pybo() : 
-        return 'Hello, Pybo!'
-    
-    return app """
+db = SQLAlchemy()
+migrate = Migrate()
     
 # 블루프린트 등록
 def create_app() : 
     app = Flask(__name__)
+    app.config.from_object(config) # config.py 파일에 작성한 항목을 읽기 위해 추가한 코드 
 
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # blueprint
     from .views import main_views
     app.register_blueprint(main_views.bp)
     
